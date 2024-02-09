bfc1ceab471477e4efebbd75dbf352b1d1a5ab14
zackaryO <90364473+zackaryO@users.noreply.github.com>
2024-02-09 15:07:49 -0700

    more

--- back_end/back_end/urls.py
+++ back_end/back_end/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('', include('lots.urls')),
+    path('api/', include('lots.urls')),
 
 ]
 
