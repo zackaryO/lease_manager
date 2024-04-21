6e8fd52af5f12d1b2304af598aa67310f0b4bbfe
Zack Otterstrom <90364473+zackaryO@users.noreply.github.com>
2024-04-20 19:50:22 -0600

    formatted reports page to filter results by date and/or name

--- lease-manager/src/app/reports/reports.component.ts
+++ lease-manager/src/app/reports/reports.component.ts
@@ -16,16 +16,22 @@ import { SelectionModel } from '@angular/cdk/collections';
 })
 export class ReportsComponent implements OnInit {
   payments: any[] = [];
+  filteredPayments: any[] = [];
   selectedPayments: any[] = [];
   selection = new SelectionModel<any>(true, []);
+  startDate: string | undefined;
+  endDate: string | undefined;
+  selectedLeaseholderId: string | undefined;
+
   constructor(private paymentService: PaymentService, private datePipe: DatePipe, private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.paymentService.getPayments().subscribe(data => {
       this.payments = data;
+      this.filteredPayments = data;
+      this.getUniqueLeaseholders();
     });
   }
-
 
 
   toggleSelection(payment: any) {
@@ -37,18 +43,33 @@ export class ReportsComponent implements OnInit {
     }
   }
 
+  applyFilters(): void {
+    this.filteredPayments = this.payments.filter(payment => {
+      const paymentDate = new Date(payment.payment_date);
+      const start = this.startDate ? new Date(this.startDate) : new Date(0);
+      const end = this.endDate ? new Date(this.endDate) : new Date();
+      const matchesDate = paymentDate >= start && paymentDate <= end;
+      const leaseholderFullName = `${payment.lease_holder_info.lease_holder_first_name} ${payment.lease_holder_info.lease_holder_last_name}`;
+      const selectedFullName = this.selectedLeaseholderId ? this.selectedLeaseholderId : "";
+      const matchesLeaseholder = !this.selectedLeaseholderId || leaseholderFullName === selectedFullName;
 
-  // toggleAllSelections(event: MatCheckboxChange) {
-  //   if (event.checked) {
-  //     this.payments.forEach(row => this.selection.select(row));
-  //   } else {
-  //     this.selection.clear();
-  //   }
-  // }
+      return matchesDate && matchesLeaseholder;
+    });
+  }
 
-  // toggleSelection(row: any) {
-  //   this.selection.toggle(row);
-  // }
+  getUniqueLeaseholders() {
+    const uniqueLeaseholders = new Map();
+    for (const payment of this.payments) {
+      const info = payment.lease_holder_info;
+      // Assuming each leaseholder has a unique combination of first and last name for uniqueness
+      const id = `${info.lease_holder_first_name} ${info.lease_holder_last_name}`;
+      if (!uniqueLeaseholders.has(id)) {
+        uniqueLeaseholders.set(id, info);
+      }
+    }
+    return Array.from(uniqueLeaseholders.values());
+  }
+
 
   deleteSelected() {
     const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
@@ -117,33 +138,6 @@ export class ReportsComponent implements OnInit {
     }
   }
 
-
-
-  // deleteLease(lease: Lease) {
-  //   const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
-  //     width: '300px',
-  //     data: { message: 'Are you sure you want to delete this lease?' }
-  //   });
-
-  //   dialogRef.afterClosed().subscribe(result => {
-  //     if (result) {
-  //       // User clicked 'Delete' in the dialog
-  //       this.rentalService.deleteLease(lease.id).subscribe(() => {
-  //           this.loadUnoccupiedLots();
-  //           this.loadLeases();
-  //           this.loadLots();
-  //           this.leaseForm.reset();
-  //         },
-  //         error: (error) => this.showErrorDialog(error)
-  //       });
-  //     }
-  //     // If result is false, the user clicked 'Cancel' or closed the dialog, do nothing
-  //   });
-  // }
-
-
-
-
   formatPaymentDate(dateString: string): string {
     const formattedDate = this.datePipe.transform(dateString, 'MMM dd, yyyy');
     if (formattedDate === null) {
@@ -155,6 +149,18 @@ export class ReportsComponent implements OnInit {
 
 
   printReport() {
+    // window.print();
+    // Add a class or directly set styles to hide the navbar
+    document.querySelector('app-navbar')?.setAttribute('style', 'display: none;');
+    document.querySelector('.actions')?.setAttribute('style', 'display: none;');
+
+    // Trigger the print dialogue
     window.print();
+
+    // Optionally reset styles after printing
+    window.setTimeout(() => {
+      document.querySelector('app-navbar')?.removeAttribute('style');
+      document.querySelector('.actions')?.removeAttribute('style');
+    }, 1000); // Delay might need adjustment based on your needs
   }
 }
